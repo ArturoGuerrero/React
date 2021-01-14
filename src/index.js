e070fc767faf04e027de9a6f10c36fb697b0cfcb
ArturoGuerrero <38035740+ArturoGuerrero@users.noreply.github.com>
2021-01-14 10:04:17 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+//Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
+//It should display 2 paragraph HTML elements.
+ReactDOM.render(
+  <div>
+    <p>Created by Angela Yu</p>
+    <p>Copyright 2019.</p>
+  </div>,
+  document.getElementById("root")
+);
